c07fc1daea1d9ecfb60091882a2578b89e6043c8
studyofcreativity <study.of.creativity@gmail.com>
2025-10-04 19:12:04 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("createVR").addEventListener("click", () => {
+  alert("The 'Create VR' feature will let you design VR scenes soon!");
+});
+
+document.getElementById("playVR").addEventListener("click", () => {
+  alert("The 'Play VR' feature will allow you to experience VR content!");
+});
